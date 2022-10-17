f8920a7006c9f68b007c861aecb94c2034d36c23
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-10-17 12:13:43 +0300

    yeeeih i gat this

---
+++ app.rb
@@ -0,0 +1,13 @@
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+
+
+print "Hello World!"       #outputs data as a single sentence without breaks
+print "Hello World!"
+print "Hello World!"
+
+p [1,2,3]   #uses the .inspect method on simple arrays.
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }] #uses .pretty-inspect method to output complex arrays.
+
+print "Pass this test, please."
